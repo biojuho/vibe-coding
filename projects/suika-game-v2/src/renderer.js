6b77a4244267f379282c9997941c6e35f68577d2
Joolife <joolife@joolife.io.kr>
2026-06-04 21:12:19 +0900

    chore(suika-game-v2): T-1240 bump eslint

--- projects/suika-game-v2/src/renderer.js
+++ projects/suika-game-v2/src/renderer.js
@@ -203,21 +203,19 @@ function updateAndDrawParticles(ctx) {
 // Helper for Star shape
 function drawStar(ctx, cx, cy, spikes, outerRadius, innerRadius) {
 	let rot = (Math.PI / 2) * 3;
-	let x = cx;
-	let y = cy;
 	const step = Math.PI / spikes;
 
 	ctx.beginPath();
 	ctx.moveTo(cx, cy - outerRadius);
 	for (let i = 0; i < spikes; i++) {
-		x = cx + Math.cos(rot) * outerRadius;
-		y = cy + Math.sin(rot) * outerRadius;
-		ctx.lineTo(x, y);
+		const outerX = cx + Math.cos(rot) * outerRadius;
+		const outerY = cy + Math.sin(rot) * outerRadius;
+		ctx.lineTo(outerX, outerY);
 		rot += step;
 
-		x = cx + Math.cos(rot) * innerRadius;
-		y = cy + Math.sin(rot) * innerRadius;
-		ctx.lineTo(x, y);
+		const innerX = cx + Math.cos(rot) * innerRadius;
+		const innerY = cy + Math.sin(rot) * innerRadius;
+		ctx.lineTo(innerX, innerY);
 		rot += step;
 	}
 	ctx.lineTo(cx, cy - outerRadius);
